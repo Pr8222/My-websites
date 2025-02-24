d27bc9791fb8c47011614017166c275eed18f42c
Pr8222 <parsa.rohani82@gmail.com>
2025-02-24 11:34:05 +0330

    remove debug statements from user.js

--- Login/FrontEnd/JS/user.js
+++ Login/FrontEnd/JS/user.js
@@ -73,9 +73,6 @@ $(document).ready(function () {
         Object.keys(updatedData).forEach((key) => {
           if (!updatedData[key]) delete updatedData[key];
         });
-
-        console.log(updatedData);
-        debugger;
         // Now, send the PUT request
         $.ajax({
           type: "PUT",
